430ac74f90674810ff49376d303a13d379eccc1d
baizhi958216 <1475289190@qq.com>
2022-10-26 21:52:37 +0800

    实现前一页/后一页
    Signed-off-by: baizhi958216 <1475289190@qq.com>

--- src/UI/controllers.js
+++ src/UI/controllers.js
@@ -1,5 +1,5 @@
 /*
-Pages: 页面大小
+Pages: 页面大小/每页显示行数
 Page: 当前页数
 */
 let DOMReRender, TBodyTitle, Pages, Page, t_Page
@@ -9,9 +9,12 @@ new Promise(() => {
     const xmlParser = new DOMParser()
     const xmldoc = xmlParser.parseFromString(res, 'text/xml')
     // 标题添加升序/降序按钮
-    DOMReRender = xmldoc.children[0].children
+    DOMReRender = Array.from(xmldoc.children[0].children)
     TBodyTitle = DOMReRender[0]
     TBodyTitle.children.age.innerHTML += '<button onclick="asc()">升序</button><button onclick="des()">降序</button>'
+    DOMReRender.map((ele, index) => {
+      ele.id = index
+    })
     // 初始页面完整渲染XML
     for (let index = 0; index < DOMReRender.length; index++) {
       document.querySelector('#books').innerHTML += DOMReRender[index].outerHTML
@@ -29,7 +32,7 @@ pageController = (e) => {
         Page = 1
         document.querySelector('#books').innerHTML = TBodyTitle.outerHTML
         for (let i = 1; i <= Pages; i++) {
-          document.querySelector('#books').innerHTML += DOMReRender[i].innerHTML
+          document.querySelector('#books').innerHTML += DOMReRender[i].outerHTML
         }
         break
       // 最后页
@@ -39,27 +42,35 @@ pageController = (e) => {
         t_Page = (DOMReRender.length - 1) - ((DOMReRender.length - 1) - ((Math.ceil((DOMReRender.length - 1) / Pages) - 1) * Pages))
         document.querySelector('#books').innerHTML = TBodyTitle.outerHTML
         for (let i = DOMReRender.length - 1; i > t_Page; i--) {
-          document.querySelector('#books').innerHTML += DOMReRender[i].innerHTML
+          document.querySelector('#books').innerHTML += DOMReRender[i].outerHTML
         }
         break
       // 前一页
       case 2:
         if (Page > 1) {
-          // 分成几页: Math.ceil((DOMReRender.length - 1) / Pages)
+          // trs 前一页第一列id
+          let trs = +document.querySelectorAll('tr')[document.querySelectorAll('tr').length - Pages].id-Pages
           Page--
-          console.log('当前页: ', Page)
           document.querySelector('#books').innerHTML = TBodyTitle.outerHTML
-          // document.querySelector('#books').innerHTML += DOMReRender[Page].innerHTML
-          // console.log(Page);
+          for (let index = trs; index < trs + Pages; index++) {
+            document.querySelector('#books').innerHTML += DOMReRender[index].outerHTML
+          }
         }
         break
       // 后一页
       case 3:
+        // trs 当前页最后列id
         if (Page < Math.ceil((DOMReRender.length - 1) / Pages)) {
+          let trs = +document.querySelectorAll('tr')[document.querySelectorAll('tr').length - 1].id
           Page++
-          console.log('当前页: ', Page)
           document.querySelector('#books').innerHTML = TBodyTitle.outerHTML
-          // document.querySelector('#books').innerHTML += DOMReRender[Page].innerHTML
+          for (let index = trs + 1; index <= trs + Pages; index++) {
+            if (DOMReRender[index]) {
+              document.querySelector('#books').innerHTML += DOMReRender[index].outerHTML
+            }else{
+              document.querySelector('#books').innerHTML += `<tr id=${index}></tr>`
+            }
+          }
         }
         break
       default:
@@ -86,9 +97,9 @@ customPage = (c_page) => {
   if (input > 0 && input < DOMReRender.length) {
     document.querySelector('#books').innerHTML = TBodyTitle.outerHTML
     for (let i = 1; i <= input; i++) {
-      document.querySelector('#books').innerHTML += DOMReRender[i].innerHTML
+      document.querySelector('#books').innerHTML += DOMReRender[i].outerHTML
     }
-    Pages = input
+    Pages = +input
     Page = 1
   } else if (input = 0) {
     document.querySelector('#books').innerHTML = TBodyTitle.outerHTML
